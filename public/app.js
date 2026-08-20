6179b1e76ff3f565f290f268aa13b2bd3f1f0ea2
Lana Butorovic <lanabutorovic@gmail.com>
2026-08-20 13:30:34 -0400

    Collapse asset rows by default in all equipment DP views

--- public/app.js
+++ public/app.js
@@ -8872,7 +8872,7 @@ var _PROJ_LABELS={hercules:'Hercules Solar + BESS',barryrose:'Barry Rose WRF',vd
             var _lonR=_lrA.filter(function(a){return a.status!=='offrent';}).length;
             var _loffR=_lrA.length-_lonR;
             var _lisOff=row.state==='Off-rent';
-            h+='<div id="'+_lpId+'" class="gas-panel open" style="border-top:1px solid var(--g100)">';
+            h+='<div id="'+_lpId+'" class="gas-panel" style="border-top:1px solid var(--g100)">';
             h+='<div class="gas-panel-hd" style="margin-left:0;padding:5px 14px">';
             if(_lonR>0)h+='<span class="gas-badge gas-badge-onrent">● '+_lonR+' on-rent</span>';
             if(_loffR>0)h+='<span class="gas-badge gas-badge-offrent">✓ '+_loffR+' historical</span>';
